6c347ed6a850fe3c55f610d78e74615a72631142
Marat <zinatulinmarat@gmail.com>
2023-06-26 20:21:58 +0500

    next commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    SECRET: process.env.SECRET
+  }
 }
 
 module.exports = nextConfig
