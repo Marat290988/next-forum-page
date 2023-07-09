b4445eddf64221bd353ba032bac33a118603cb53
Marat <zinatulinmarat@gmail.com>
2023-07-09 20:20:07 +0500

    next commit

--- src/components/home-main/section-item/add-forum/AddForum.tsx
+++ src/components/home-main/section-item/add-forum/AddForum.tsx
@@ -1,13 +1,24 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import styles from './AddForum.module.scss'
 import { MyForm } from '@/components/ui/MyForm/MyForm';
 
 export const AddForum: FC = () => {
 
+  const refData = useRef<any>(null);
+
+  const submit = (text: string) => {
+    refData.current?.clearInput();
+  }
+
   return (
     <div>
       Add Forum
-      <MyForm />
+      <MyForm 
+        title='Forum name' 
+        submitFunc={submit}
+        maxLength={500}
+        ref={refData}
+      />
     </div>
   );
 }
--- src/components/ui/MyForm/MyForm.tsx
+++ src/components/ui/MyForm/MyForm.tsx
@@ -1,35 +1,52 @@
-import { FC } from 'react';
+import { FC, ForwardedRef, forwardRef } from 'react';
 import styles from './MyForm.module.scss';
 import { useMemo } from 'react';
 import { useState } from 'react';
+import { AiOutlinePlus  } from 'react-icons/ai';
+import { FormEvent } from 'react';
 
-export const MyForm: FC = () => {
-  const uniqId = useMemo(() => crypto.randomUUID(), []);
-  const [any, setAny] = useState(false);
-
-  return (
-    <form
-      className={`${styles['my-form']} border-color-dark`}
-      onClick={() => setAny(true)}
-    >
-      <div className='flex'>
-        <div
-          className='grow'
-        >
-              <label htmlFor={`section${uniqId}`}>Section name:</label>
-              <div>
-                <input
-                  id={`section${uniqId}`}
-                  className={styles.input}
-                />
-              </div>
-        </div>
-      </div>
-      <div
-        className={`${styles['my-form-counter']}`}
+export const MyForm: FC<{title: string, submitFunc: (text: string) => void, maxLength: number, clear?: () => void, ref: any}> =
+  forwardRef(({title, submitFunc, maxLength}, ref: any) => {
+    const uniqId = useMemo(() => crypto.randomUUID(), []);
+    const [formText, setFormText] = useState('');
+    const inputHandler = (e: FormEvent<HTMLInputElement>) => {
+      const el: HTMLInputElement = e.target as HTMLInputElement;
+      setFormText(el.value.substring(0, maxLength));
+    };
+    ref.current = {clearInput: () => setFormText('')};
+  
+    return (
+      <form
+        className={`${styles['my-form']} border-color-dark`}
       >
-
-      </div>
-    </form>
-  )
-}
+        <div className='flex'>
+          <div
+            className='grow'
+          >
+                <label htmlFor={`r${uniqId}`}>{title}:</label>
+                <div>
+                  <input
+                    id={`r${uniqId}`}
+                    className={styles.input}
+                    value={formText}
+                    onInput={inputHandler}
+                    onPaste={inputHandler}
+                  />
+                </div>
+          </div>
+          <div
+            className={`px-[10px] flex items-center ${styles["check-button"]}`}
+            onClick={() => submitFunc(formText)}
+          >
+            <AiOutlinePlus />
+          </div>
+        </div>
+        <div
+          className={`${styles['my-form-counter']}`}
+        >
+          {formText.length} / {maxLength}
+        </div>
+      </form>
+    )
+    
+  })
