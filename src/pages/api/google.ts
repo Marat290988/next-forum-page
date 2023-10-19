63a81492cf5b25a9943263afc961e434eaae34c3
marat.zinatulin <marat.zinatulin@fido.uz>
2023-10-19 15:07:33 +0500

    next commit

--- src/pages/api/google.ts
+++ src/pages/api/google.ts
@@ -1,29 +1,75 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-// const {OAuth2Client} = require('google-auth-library');
+import { OAuth2Client } from "google-auth-library";
+import { axiosReq } from "@/axios/api";
+import { Prisma } from "@/utils/prisma";
+import { hash } from "argon2";
+import { signJwt } from "@/utils/token";
+import { serialize } from 'cookie';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
 
-  console.log(req.query)
-  console.log(req.url)
+  if (req.method !== 'GET') {
+    res.status(403).json({message: 'Only GET requests allowed.'});
+    return;
+  }
 
-  // const client = new OAuth2Client();
-  // async function verify() {
-  //   const ticket = await client.verifyIdToken({
-  //       idToken: 'token',
-  //       audience: '683786685127-lbfh155lvus59p6omf23cbvq15cep823.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
-  //       // Or, if multiple clients access the backend:
-  //       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  //   });
-  //   const payload = ticket.getPayload();
-  //   const userid = payload['sub'];
-  //   console.log(payload)
-  //   // If request specified a G Suite domain:
-  //   // const domain = payload['hd'];
-  // }
-  //verify().catch(console.error);
-  res.status(200).json({message: 'OK'})
+  const token = req.query.token;
+  
+  if (!token) {
+    res.status(403).json({message: 'Missing token.'});
+  }
+
+  axiosReq({url: `https://www.googleapis.com/oauth2/v2/tokeninfo?access_token=${token}`})
+    .then(async resG => {
+      if (resG.data.expires_in > 0) {
+        const prisma = Prisma.getPrisma();
+        try {
+          const oldEmail = await prisma.user.findUnique({
+            where: {email: resG.data.email}
+          });
+          let resUser: any;
+          if (oldEmail) {
+            resUser = {...oldEmail};
+          } else {
+            resUser =  await prisma.user.create({
+              data: {
+                email: resG.data.email,
+                name: resG.data.email.split('@')[0],
+                password: await hash('rijifjihjfrgihgjirhhdfgkjfdngkgdfg654654g65df4g654df65g4d6f45g65fd4g6d4f65g4fd6g54fd654g65df4g')
+              }
+            });
+          }
+
+          delete resUser.password;
+          const tokenMaxAge = (new Date().getTime()); // 1 week
+
+          const token: string = await signJwt(
+            { sub: JSON.stringify(resUser) },
+            { exp: `7d` }
+          );
+      
+          const cookieOptions = {
+            httpOnly: false,
+            path: "/",
+            secure: false,
+            maxAge: tokenMaxAge,
+          };
+      
+          res.setHeader('Set-Cookie', serialize('token', token, {...cookieOptions}));
+          prisma.$disconnect();
+          res.status(200).json({user: resUser, message: 'Successful logging.'});
+        } catch {
+          prisma.$disconnect();
+          res.status(422).json({message: 'Problems with DB.'});
+          return;
+        }
+      }
+    }).
+    catch(() => {
+      res.status(422).json({message: 'Problems with server.'});
+    });
+
 }
--- src/pages/wait/index.tsx
+++ src/pages/wait/index.tsx
@@ -1,14 +1,24 @@
+import { axiosReq } from "@/axios/api";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 const WaitPage = () => {
   const router = useRouter();
 
+  const googleCheckToken =  useCallback(async (token: string) => {
+    const res = await axiosReq({
+      url: `google?token=${token}`
+    });
+    console.log(res.data)
+  }, [])
+
   useEffect(() => {
     const hash = (router.asPath as string).split("#")[1]; // error=unauthorized_client&error_code=401error_description=Something+went+wrong
     const parsedHash = new URLSearchParams(hash);
-    console.log(Object.fromEntries(parsedHash))
-  }, [])
+    // console.log(parsedHash.get('access_token'))
+    googleCheckToken(parsedHash.get('access_token') as string);
+    
+  }, [googleCheckToken])
   
 }
 
